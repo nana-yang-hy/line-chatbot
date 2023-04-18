4e9aca9c1d854ff6961f025330b7b10b79cf6679
Huiyu Yang <verthandi0105@gmail.com>
2023-04-18 10:31:19 +0800

    refactor: 使用line/bot-sdk重構程式碼

--- open-ai-chat-api/app.js
+++ open-ai-chat-api/app.js
@@ -1,97 +1,68 @@
-const { Configuration, OpenAIApi } = require("openai");
+const line = require("@line/bot-sdk");
+const express = require("express");
+const app = express();
+const {Configuration, OpenAIApi} = require("openai");
 require("dotenv").config();
 
-//Line channel access token and secret
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
+const client = new line.Client({
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
 
 //openAI API key
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    organization: process.env.OPENAI_ORGANIZATION_ID,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-async function generateChatResponse(message) {
-  try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Human:" + message,
-      temperature: 0.9,
-      max_tokens: 150,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.6,
-      stop: [" Human:", " AI:"],
-    });
-    return response.data.choices[0].text;
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
+// use GPT3.5 chatAPI create chat
+async function generateChatResponse(message){
+    try {
+        console.log(`user: ${message}`);
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: message}],
+        });
+        let result = completion.data.choices[0].message;
+        console.log(result);
+        return result;
+    } catch (e) {
+        console.error(e);
+        return e;
     }
-    return "error";
-  }
-}
-
-// async function handleLineBotEvent(event) {
-//   const message = event.message.text;
-//   const response = await generateChatResponse(message);
-//   console.log("Received message:", message);
-//   const replyMessage = {
-//     type: "text",
-//     text: response,
-//   };
-//   console.log("openAI response:", response);
-//   await lineClient.replyMessage(event.replyToken, replyMessage);
-// }
-
-const express = require("express");
-const app = express();
-// const port = process.env.PORT || 8000;
+};
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-// app.post("/", (req, res) => {
-//   Promise.all(req.body.events.map(handleLineBotEvent)).then((result) => {
-//     res.json(result);
-//   });
-// });
+// post user's message to get chat response
 app.post("/", async (req, res) => {
-  let message = req.body.events[0].message.text;
-  let result = await generateChatResponse(message);
-
-  let bodyData = {
-    replyToken: req.body.events[0].replyToken,
-    messages: [
-      {
-        type: "text",
-        text: result,
-      },
-    ],
-  };
-  console.log(bodyData);
-  fetch("https://api.line.me/v2/bot/message/reply", {
-    body: JSON.stringify(bodyData),
-    headers: {
-      Authorization:
-        "Bearer GpdKfQgZK8lGSUuJrep+t2flrMgYz03NP5WPTEPZV/MOtMSPDCq1AThEF9lJLcv1vkDUKyF38uyLSE3V0GcSweFc1uHsNoFfqaFYxMDRVvOBoRmlnE3stwapZpbkaLTouqTtmE9tqjRgPFyele35OwdB04t89/1O/w1cDnyilFU=",
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-  })
-    .then((d) => d.json())
-    .then((result) => {
-      console.log(result);
-    });
-  console.log(JSON.stringify(req.body));
-  return res.send({});
+    try {
+        console.log(JSON.stringify(req.body));
+        let eventType = req.body.events[0].type;
+        if(eventType === "follow") {
+            let replyToken = req.body.events[0].replyToken;
+            await client.replyMessage(replyToken, {type:"text",text: `Hi 很高興您加我好友！ \n我是一個聊天機器人，傳送任何文字訊息開始聊天吧～`});
+            return res.status(200);
+        }
+        let messageType = req.body.events[0].message.type;
+        if(messageType !== "text") {
+            let replyToken = req.body.events[0].replyToken;
+            await client.replyMessage(replyToken, {type:"text",text: "抱歉，我只能處理文字訊息"});
+            return res.status(200);
+        }
+        let message = req.body.events[0].message.text;
+        let result = await generateChatResponse(message);
+        let replyToken = req.body.events[0].replyToken;
+        await client.replyMessage(replyToken, {type:"text",text: result.content});
+        return res.status(200);
+    } catch (e) {
+        console.log('error');
+        // console.error(e);
+        return res.status(400);
+    }
 });
 
 app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+    console.log("Server is running on port 8000");
 });
